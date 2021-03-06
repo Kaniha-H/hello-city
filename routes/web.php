f9117c3ddb4c1fe2dc67c9298f29ec232c3ec10a
HENG Kaniha <hengkaniha@gmail.com>
2021-03-06 23:18:01 +0100

    Use Route::view

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages/home');
-})->name('app_home');
+/* Ça fonctionne */ 
+// Route::get('/', function () {
+//     return view('pages/home');
+// })->name('app_home');
 
-Route::get('/about-us', function () {
-    return view('pages/about-us');
-})->name('app_about');
+// Route::get('/about-us', function () {
+//     return view('pages/about-us');
+// })->name('app_about');
+
+/* Ne fonctionne pas */ 
+// Route::get('/', fn() => view('pages/home'))->name('app_home');
+// Route::get('/about-us', fn() => view('pages/about-us'))->name('app_about');
+
+Route::view('/', 'pages.home')->name('app_home');
+Route::view('/about-us', 'pages.about-us')->name('app_about');
